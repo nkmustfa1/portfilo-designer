e9aa63d4f3dc87741f81649a078865e161a67a23
nkmustfa1 <nkmohammed27@gmail.com>
2026-01-27 03:18:07 +0300

    Improve mobile background and add bottom gradient via CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./i18n";
 import { LanguageProvider } from "@/context/LanguageContext";
 
 createRoot(document.getElementById("root")!).render(
